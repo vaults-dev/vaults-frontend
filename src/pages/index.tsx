30f17750f68adf3a1b57fbee30211a51f131b370
Yohanes Kiolol <yohanes.handratas@computradetech.com>
2023-06-10 12:13:56 +0700

    uninstall icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,3 @@
-import { useForm } from 'react-hook-form'
-import axios from 'axios'
-
 export default function Home() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm()
-  const onSubmit = async (data: any) => {
-    const test = await axios
-      .post('https://vaults-backend-production.up.railway.app/login', data, {
-        withCredentials: true,
-      })
-      .catch((error) => console.log(error))
-
-    const res2 = await axios.get(
-      'https://vaults-backend-production.up.railway.app/home',
-      { withCredentials: true }
-    )
-
-    console.log(res2)
-  }
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('email', { required: true })} />
-
-      <input {...register('password', { required: true })} />
-      {errors.exampleRequired && <span>This field is required</span>}
-
-      <input type="submit" />
-    </form>
-  )
+  return <h1>Hello Worlds!</h1>
 }
